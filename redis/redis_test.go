2972dfec62bc1efb3f929bf2668325ed7e9edd86
Phil <zgrubby@gmail.com>
2016-03-30 14:51:09 +0800

    Format test

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -7,17 +7,6 @@ import (
 	"github.com/philchia/go_redis_driver/redis"
 )
 
-// func BenchmarkConnection(b *testing.B) {
-// 	log.Println("redis proxy connection")
-// 	for i := 0; i < b.N; i++ {
-// 		conn, err := redis.Connect("127.0.0.1:6379", "112919147")
-// 		if err != nil {
-// 			log.Println(err.Error())
-// 		}
-// 		conn.Close()
-// 	}
-// }
-
 func BenchmarkSetKey(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		conn, err := redis.Connect("127.0.0.1:6379", "112919147")
